c98b0d935f4ade6ea8af120e6e0ca027afbeef21
y11n <samtori3849@gmail.com>
2024-11-04 21:24:32 +0900

    초기 세팅

---
+++ src/test/java/team8/nugu/NuguApplicationTests.java
@@ -0,0 +1,13 @@
+package team8.nugu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
