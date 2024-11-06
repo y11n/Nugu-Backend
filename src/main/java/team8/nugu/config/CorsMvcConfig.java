89950b81050e08b8b46c2fe91981282b19713f43
y11n <samtori3849@gmail.com>
2024-11-06 23:35:05 +0900

    [Feat] CORS 설정

---
+++ src/main/java/team8/nugu/config/CorsMvcConfig.java
@@ -0,0 +1,16 @@
+package team8.nugu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 컨트롤러단의 CORS 설정
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+}
